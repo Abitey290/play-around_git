7e451a4f4db6b78ac73a9d5ca4fc0a9556ecf276
Isaac Abitey <abiteyisaac290@gmail.com>
2021-08-12 16:22:39 +0100

    Python program

---
+++ program_box/python/user.py
@@ -0,0 +1,4 @@
+# Ask for username and print message to thank them for providing their names
+
+user = input("Please Enter your full name:  ")
+print(f"Hello, {user}, thank you for providing your name")
